8f0ddf4d8f3e1bb6cfde0e5b60872398a86adc90
Chris Olstrom <chris@olstrom.com>
2016-03-01 07:35:08 -0800

    Add #path to Support::Params

--- lib/github-status/support/params.rb
+++ lib/github-status/support/params.rb
@@ -5,6 +5,14 @@ module GitHubStatus
     module Params
       include ::Contracts::Core
       include ::Contracts::Builtin
+
+      Contract None => String
+      def path
+        @path ||= params.fetch 'path'
+      rescue KeyError
+        STDERR.puts 'Params is missing path'
+        abort
+      end
     end
   end
 end
