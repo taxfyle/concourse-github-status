9b99124722a4b0647d70640456256b459e8130b0
Chris Olstrom <chris@olstrom.com>
2016-02-29 22:34:59 -0800

    Add #repo to Support::Source

--- lib/github-status/support/source.rb
+++ lib/github-status/support/source.rb
@@ -5,6 +5,14 @@ module GitHubStatus
     module Source
       include ::Contracts::Core
       include ::Contracts::Builtin
+
+      Contract None => String
+      def repo
+        @repo ||= source.fetch 'repo'
+      rescue KeyError
+        STDERR.puts 'Source is missing repo'
+        abort
+      end
     end
   end
 end
