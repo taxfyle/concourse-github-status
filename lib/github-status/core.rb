9a314749aa77319e10a780675c9dc569b01d065a
Chris Olstrom <chris@olstrom.com>
2016-02-29 22:35:22 -0800

    Add Support::Source to Core

--- lib/github-status/core.rb
+++ lib/github-status/core.rb
@@ -1,8 +1,10 @@
 require 'contracts'
+require_relative 'support/source'
 
 module GitHubStatus
   module Core
     include ::Contracts::Core
     include ::Contracts::Builtin
+    include ::Support::Source
   end
 end
