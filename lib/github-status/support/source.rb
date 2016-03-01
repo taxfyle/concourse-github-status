3c58d86190b51c6902c3e616a71efa1caf5929e5
Chris Olstrom <chris@olstrom.com>
2016-02-29 22:37:49 -0800

    Add #access_token to Support::Source

--- lib/github-status/support/source.rb
+++ lib/github-status/support/source.rb
@@ -7,6 +7,14 @@ module GitHubStatus
       include ::Contracts::Builtin
 
       Contract None => String
+      def access_token
+        @access_token ||= source.fetch 'access_token'
+      rescue KeyError
+        STDERR.puts 'Source is missing access_token'
+        abort
+      end
+
+      Contract None => String
       def repo
         @repo ||= source.fetch 'repo'
       rescue KeyError
