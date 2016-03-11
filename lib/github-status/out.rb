4bbc26059a1c67ade6442d6bdeaf3009742e9b11
Chris Olstrom <chris@olstrom.com>
2016-03-11 15:39:57 -0800

    Remove #git from GitHubStatus::Out

--- lib/github-status/out.rb
+++ lib/github-status/out.rb
@@ -41,14 +41,6 @@ module GitHubStatus
       }
     end
 
-    Contract None => Git::Base
-    def git
-      @git ||= Git.open "#{workdir}/#{path}"
-    rescue ArgumentError
-      STDERR.puts "#{path} is not a git repository"
-      abort
-    end
-
     Contract None => String
     def sha
       @sha ||= git.revparse 'HEAD'
