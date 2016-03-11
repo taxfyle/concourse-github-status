4d360d92466f045722fc90fd183d42de97e12a53
Chris Olstrom <chris@olstrom.com>
2016-03-11 15:44:38 -0800

    Add #github to Support::GitHub

--- lib/github-status/support/github.rb
+++ lib/github-status/support/github.rb
@@ -1,10 +1,16 @@
 require 'contracts'
+require 'octokit'
 
 module GitHubStatus
   module Support
     module GitHub
       include ::Contracts::Core
       include ::Contracts::Builtin
+
+      Contract None => Octokit::Client
+      def github
+        @github ||= Octokit::Client.new access_token: access_token
+      end
     end
   end
 end
