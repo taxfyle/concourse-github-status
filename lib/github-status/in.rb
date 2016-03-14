9ab12f38420d691d0f847d45d54475e285547da9
Chris Olstrom <chris@olstrom.com>
2016-03-14 13:37:36 -0700

    Make In do something useful

--- lib/github-status/in.rb
+++ lib/github-status/in.rb
@@ -1,6 +1,27 @@
 require 'concourse-fuselage'
+require 'contracts'
+require_relative 'core'
+require_relative 'support/git'
+require_relative 'support/github'
+require_relative 'support/params'
 
 module GitHubStatus
   class In < Fuselage::In
+    include Core
+    include Support::Params
+    include Support::Git
+    include Support::GitHub
+
+    def status
+      github
+        .statuses(repo, sha)
+        .select { |status| status.context == context }
+        .map(&:state)
+        .first
+    end
+
+    def fetch!
+      File.write "#{workdir}/#{context}.state", state
+    end
   end
 end
